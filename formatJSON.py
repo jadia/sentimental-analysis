0cb99f5385911249aa6c2266ee8d9c707f11ec75
w4rb0y <iamnj95@gmail.com>
2019-03-15 09:22:07 +0530

    Format extracted JSON data
    Format extracted json data so python json module can process it.

---
+++ formatJSON.py
@@ -0,0 +1,18 @@
+import json
+
+
+class formatJSON():
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+    def formatJSON(self):
+        newJSON = open("newJSON.json", 'w')
+        newJSON.write('[ ')
+        with open(self.fileName, 'r') as infile:
+            data = infile.read()
+            new_data = data.replace('}\n{', '},\n{')
+            json_data = json.loads(f'[{new_data}]')
+            print(json_data)
+            newJSON.write(new_data)
+        newJSON.write(' ]')
+        newJSON.close()
