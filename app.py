52901a9ddf8500a280460bf0be5663ec516e9d76
w4rb0y <iamnj95@gmail.com>
2019-03-20 07:52:38 +0530

    Add stub to get final result
    Add stub to get final result
    
    Add stub to get final result

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, redirect, flash, url_for, session, logging, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-from unitTest import TweepyUnitTest
+from gettweets import AutomateAll
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
@@ -15,8 +15,9 @@ def index():
         keyword = request.form['keyword']
         #flash('Requested keywords: {}'.format(keyword))
         keywordAsList = Keyword2List(keyword)
-        analysis = TweepyUnitTest(keywordAsList.keyword2List())
-        return render_template('result.html', keyword=keywordAsList, result=analysis.analyse())
+        #analysis = TweepyUnitTest(keywordAsList.keyword2List())
+        analysis = AutomateAll(keywordAsList.keyword2List())
+        return render_template('result.html', keyword=keywordAsList, result=analysis.extractStructTweets())
     else:
         # keyword = request.form['keyword']
         # if len(keyword) == 0:
